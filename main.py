02546d469d57a6bbddc77b87dcc7933efc60881d
Rohit Gite <rohitgite1419@gmail.com>
2025-12-16 19:07:37 +0530

    Initialize the FastAPI server

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from videochat!")
+from fastapi import FastAPI
+from dotenv import load_dotenv
+
+# load env variables from .env file
+load_dotenv()
+
+app = FastAPI(
+    title = "Video Chat LLM agent",
+    description = "A FastAPI application for video interaction with LLM agents.",
+    version = "1.0.0"
+)
+
+
+@app.get("/")
+def health_check():
+    return {"status": "ok",
+            "service": "Video Chat LLM agent is running.",
+            "version": "1.0.0"
+            }
 
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+
+    uvicorn.run("main:app", host= "0.0.0.0", port =8000, reload=True)
